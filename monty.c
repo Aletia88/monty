e7e16b109da5354d02c997cb82e0908094a5643d
Samrawit Girma <abababmgmgmg@gmail.com>
2022-09-08 13:14:24 -0700

    monty

--- monty.c
+++ monty.c
@@ -10,10 +10,10 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file/n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	cmd = open(arg[1], O_RDONILY);
+	cmd = open(argv[1], O_RDONLY);
 	if (cmd == -1)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	{
 		free(size);
 		close(cmd);
-		exit(EXIT_FAILURE);						
+		exit(EXIT_FAILURE);
 	}
 	token = strtok(size, "\n ");
 	while (token != NULL)
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
 		{
 			push(&inst, line, token);
 			line_count = 0;
-			token = strtok(NULL, "\m");
+			token = strtok(NULL, "\n ");
 			line++;
 			continue;
 		}
@@ -52,19 +52,19 @@ int main(int argc, char *argv[])
 		{
 			if (opCode(token) != 0)
 			{
-				opCode(token) (&inst, line);
+				opCode(token)(&inst, line);
 			}
 			else
 			{
 				free_dlist(&inst);
-				fprintf(stderr, "L:%d: unknown instruction %s\n". line, token);
+				fprintf(stderr, "L%d: unknown instruction %s\n", line, token);
 				exit(EXIT_FAILURE);
 			}
 		}
 		line++;
 		token = strtok(NULL, "\n ");
 	}
-	free_dlist(&linst);
+	free_dlist(&inst);
 	free(size);
 	close(cmd);
 	return (0);
